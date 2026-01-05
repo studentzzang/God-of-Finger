17cb1a91f15f197e8687e6778f518cdcd4ef344f
studentzzang <studentzzang@gmail.com>
2026-01-06 01:34:19 +0900

    손가락으로 집기 구현 완성 / TODO : 스크롤 힘조절 구현 , 이미 손가락을 오므린 채로 물체 집어올리는 것 방지

--- Assets/Scripts/Movement/HoldInteraction.cs
+++ Assets/Scripts/Movement/HoldInteraction.cs
@@ -1,15 +1,25 @@
 ///<summary>
-/// 미니게임-털 오브젝트에 부착
+/// 잡히는 대상 게임 오브젝트에 부착
+/// 잡히느 대상 오브젝트는 트리거 콜라이더 필수 부착
 ///</summary>
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HairInteraction : MonoBehaviour
 {
     public Transform targetHand; //손 오브젝트
     Vector3 offset;
+
+    public float _holdTimerLimit = 0.4f;
+    public float _holdTimer = 0;
+
+    public float _failTimer = 0;
+    public float _failTimerLimit = 1f;
+
+    public bool _catchState = true; //잡기 성공 or 실패
 
     private int _collide = 0; // 두 손가락 모두 충돌 -> 2되면 잡히기
     private Vector2 _initialPos;
@@ -40,6 +50,7 @@ public class HairInteraction : MonoBehaviour
     void LateUpdate()
     {
         CheckCatch();
+        Debug.Log((_holdTimer, _catchState, _collide));
     }
     void SaveInitialPos() //처음 좌표 기억하기
     {
@@ -51,11 +62,47 @@ public class HairInteraction : MonoBehaviour
     {
         return transform.position - targetHand.position;
     }
+    //0일때 timer 0
+    // 1일때 timer +
+    //2 일때 
     void CheckCatch()
     {
-        if (_collide >= 2)
+        if (_collide == 0)
         {
-            transform.position = targetHand.position + offset;
+            _catchState = true;
+            _holdTimer = 0;
         }
+        if (_catchState &&  _collide == 1)
+        {
+            _holdTimer += Time.deltaTime;
+
+        }
+        if (_collide == 2)
+        {
+            if(_catchState && _holdTimer < _holdTimerLimit)
+            {
+                _holdTimer = 0;
+                _catchState = false;
+                Catched();
+            }
+            else
+            {
+                FailCatched();
+            }   
+        }
+    }
+    void FailCatched()
+    {
+        _failTimer += Time.deltaTime;
+        if(_failTimer > _failTimerLimit)
+        {
+            _catchState = true;
+            return;
+        }
+    }
+    void Catched()
+    {
+        transform.position = targetHand.position + offset;
+       
     }
 }
