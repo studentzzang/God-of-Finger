54bc3cf33a8d4f7625527b4ff21b042bde08d47a
ksihun <58981473+ksihun@users.noreply.github.com>
2026-02-08 14:09:58 +0900

    파일 추가
    .

---
+++ Assets/Scripts/Audio/AudioState.cs
@@ -0,0 +1,6 @@
+public enum AudioState
+{
+    World,
+    Minigame,
+    Cinematic
+}
--- Assets/Scripts/Scene/SceneEnterDialogueTrigger.cs
+++ Assets/Scripts/Scene/SceneEnterDialogueTrigger.cs
@@ -1,3 +1,7 @@
+using System;
+
+// If QuestState and QuestManager are in a namespace, add its using directive:
+// using YourQuestNamespace;
 using System.Collections;
 using UnityEngine;
 
@@ -12,6 +16,12 @@ public class SceneEnterDialogueTrigger : MonoBehaviour
 
     [Header("Timing")]
     [SerializeField] private int delayFrames = 1;
+
+    [Header("Quest Condition (optional)")]
+    [SerializeField] private bool useQuestGate = false;
+    [SerializeField] private string requiredQuestId = "";
+    [SerializeField] private QuestState requiredState = QuestState.Completed;
+    [SerializeField] private bool requiredAtLeast = true;
 
     private void Start()
     {
@@ -28,6 +38,16 @@ public class SceneEnterDialogueTrigger : MonoBehaviour
 
         while (DialogueManager.Instance.IsOpen)
             yield return null;
+
+        if (useQuestGate)
+        {
+            if (QuestManager.Instance == null) yield break;
+            if (string.IsNullOrEmpty(requiredQuestId)) yield break;
+
+            var cur = QuestManager.Instance.GetState(requiredQuestId);
+            bool ok = requiredAtLeast ? (cur >= requiredState) : (cur == requiredState);
+            if (!ok) yield break;
+        }
 
         if (runOnce)
         {
